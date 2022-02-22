0a671e8fcdfde1bb97967615e797aa8a5fbdad7c
rupali wakde <rupaliwakde22@gmail.com>
2022-02-22 19:11:48 +0530

    Initial Commit

---
+++ CoreDataAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataAssignment
+//
+//  Created by Apple on 22/02/22.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
